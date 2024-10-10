0fbd13b1c5a5f6eecc2edce60b12445d7c6aed8a
Adammm <adambahymaulana7@gmail.com>
2024-10-10 08:08:55 +0700

    Lulus dan Tidak Lulus

--- SiakadFor01.java
+++ SiakadFor01.java
@@ -5,19 +5,26 @@ public class SiakadFor01 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0; 
 
-        for (int i = 1; i <= 10; i++) {
+
+        for (int i = 1; i <= 10; i++) { 
             System.out.print("Masukkan nilai mahasiswa ke-" + i + " : ");
             nilai = sc.nextDouble();
-            if (nilai > tertinggi) {
-                tertinggi = nilai;
+            lulus += (nilai >= 60) ? 1 : 0;  
+            tidakLulus += (nilai < 60) ? 1 : 0; 
+            if (nilai > tertinggi) {                                        
+                tertinggi = nilai;                                          
+            }                                                               
+            if (nilai < terendah) {                                         
+                terendah = nilai;                                           
             }
-            if (nilai < terendah) {
-                terendah = nilai;
-            }
+            
         }
 
         System.out.println("Nilai tertinggi : " + tertinggi);
         System.out.println("Nilai terendah : " + terendah);
+        System.out.println("Jumlah mahasiswa lulus : " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus : " + tidakLulus);
     }
 }
